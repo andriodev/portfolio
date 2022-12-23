4a2b3599b409ec6cf7bd8a2c3660ae969ff3e888
Andrey <andrios2003@gmail.com>
2022-12-23 12:46:20 +0100

    script

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,22 @@
 const hamburger = document.querySelector('.hamburger'),
 	menu = document.querySelector('.menu'),
 	block = document.querySelector('.menu__block'),
-	clossElem = document.querySelector('.menu__close')
+	clossElem = document.querySelector('.menu__close'), 
+	link = document.querySelector('.menu__list')
 hamburger.addEventListener('click', () => {
 	menu.classList.add('active'),
 	block.classList.add('active');
 });
 
 clossElem.addEventListener('click', () => {
-	block.classList.add('active'),
+	block.classList.remove('active'),
 	menu.classList.remove('active');	
 });
+
+link.addEventListener('click', () => {
+	block.classList.remove('active'),
+	menu.classList.remove('active');
+})
 
 
 const procents = document.querySelectorAll('.use__statistic-progress'),
@@ -19,3 +25,66 @@ const procents = document.querySelectorAll('.use__statistic-progress'),
 	procents.forEach((item, i) =>{
 		lines[i].style.width = item.innerHTML;
 	});
+
+
+	$(document).ready(function(){
+
+	function validateForms (form) {
+		$(form).validate({
+			debug: true,
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				text: {
+					required: true,
+					minlength: 10
+				},
+				policy: 'required'
+			},
+			messages: {
+				name: {
+					required: 'Введите ваше имя',
+					minlength: 'Пожалйста, укажите не менее {0} символов'
+				},
+				email: {
+					required: 'Введите свою почту',
+					email: 'Некорректный адресс еллектроной почты'
+				},
+				text: {
+					required: 'Введите текст сообщения',
+					minlength: 'Минимум {0} символов'
+				}
+			}
+		});
+	};
+
+	validateForms('#contact_form');
+
+
+	$('form').submit(function(e) {
+		e.preventDeflaut();
+
+		if(!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type:'POST',
+			url: 'mailer/smart.php',
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find('input').val('');
+
+
+			$('form').trigger('reset')
+		});
+		return false;
+	});
+
+})
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -1,8 +1,9 @@
 <?php 
 
 $name = $_POST['name'];
-$phone = $_POST['phone'];
+/* $phone = $_POST['phone']; */
 $email = $_POST['email'];
+$text = $_POST['text']
 
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
@@ -13,13 +14,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'andrios2003@gmail.com';                 // Наш логин
+$mail->Password = 'cwpyrhsfqzmfjufx';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
-$mail->addAddress('');     // Add a recipient
+$mail->setFrom('andrios2003@gmail.com', 'Pulse');   // От кого письмо 
+$mail->addAddress('sadaj53688@khaxan.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
@@ -30,10 +31,7 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
-		Пользователь оставил данные <br> 
-	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
+		Какой-то идиот называт себя '. $name . '  и хочет сказать что у него '. $text .'. Послать его можно по адрессу ' . $email . '';
 
 if(!$mail->send()) {
     return false;
